50e0ce0af93b5c684f4dfafe01d16ca3ca5b0f41
gyuri127 <gyurih127@gmail.com>
2022-12-15 01:37:33 +0900

    참가자추가1

--- app/src/main/java/com/example/todaydrink/ParticipantAdapter.java
+++ app/src/main/java/com/example/todaydrink/ParticipantAdapter.java
@@ -6,45 +6,53 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+public class ParticipantAdapter extends RecyclerView.Adapter< ParticipantAdapter.ViewHolder> {
 
-public class ParticipantAdapter extends RecyclerView.Adapter<Holder> {
-    ArrayList<String> list;
+    private ArrayList<String> mData = null ;
 
+    // 아이템 뷰를 저장하는 뷰홀더 클래스.
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+
+        ViewHolder(View itemView) {
+            super(itemView) ;
+
+            // 뷰 객체에 대한 참조. (hold strong reference)
+
+        }
+    }
+
+    // 생성자에서 데이터 리스트 객체를 전달받음.
     ParticipantAdapter(ArrayList<String> list) {
-        this.list = list;
+        mData = list ;
     }
 
-    @NonNull
+    // onCreateViewHolder() - 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴.
     @Override
-    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.participant_recycler, parent, false);
-        return new Holder(view);
+    public  ParticipantAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext() ;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) ;
+
+        View view = inflater.inflate(R.layout.participant_recycler, parent, false) ;
+        ParticipantAdapter.ViewHolder vh = new  ParticipantAdapter.ViewHolder(view) ;
+
+        return vh ;
     }
 
+    // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시.
     @Override
-    public void onBindViewHolder(@NonNull Holder holder, int position) {
-        holder.name.setText(list.get(position));
+    public void onBindViewHolder( ParticipantAdapter.ViewHolder holder, int position) {
+        String text = mData.get(position) ;
+        holder.name.setText(text) ;
     }
 
-
+    // getItemCount() - 전체 데이터 갯수 리턴.
     @Override
     public int getItemCount() {
-        return list.size();
-    }
-}
-
-class Holder extends RecyclerView.ViewHolder {
-    TextView name;
-
-    public Holder(@NonNull View itemView) {
-        super(itemView);
-        name = itemView.findViewById(R.id.participant_recyclerView_name);
+        return mData.size() ;
     }
 }
